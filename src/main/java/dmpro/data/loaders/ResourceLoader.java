f0038cf6a2de69683ec0784594cef7c74d9fce95
captainjspace <joshualandman@yahoo.com>
2016-09-29 00:06:57 -0700

    moved loader package

---
+++ src/main/java/dmpro/data/loaders/ResourceLoader.java
@@ -0,0 +1,20 @@
+package dmpro.loaders;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class ResourceLoader {
+	
+	public String data_directory = System.getProperty("data.directory");
+	
+	private Logger logger;
+	private FileReader reader;
+	private String directory;
+	private String fileName;
+	
+
+}
